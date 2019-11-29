6ecdb82c0848b39b3efad687c51a6ea5de203b61
Alina Ray <a.alina.ray@gmail.com>
2019-11-29 18:29:48 +0200

    storing the address was implemented

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -12,11 +12,14 @@ const LocationPicker = props => {
 
     const mapPickedLocation = props.navigation.getParam('pickedLocation');
 
+    const {onLocationPicked} = props;
+
     useEffect(() => {
         if (mapPickedLocation) {
             setPickedLocation(mapPickedLocation);
+            onLocationPicked(mapPickedLocation);
         }
-    }, [mapPickedLocation]);
+    }, [mapPickedLocation, onLocationPicked]);
 
     const verifyPermissions = async () => {
         const result = await Permissions.askAsync(Permissions.LOCATION);
@@ -41,6 +44,10 @@ const LocationPicker = props => {
                 timeout: 5000, //promise will be rejected after 5s
             });
             setPickedLocation({
+                lat: location.coords.latitude,
+                lng: location.coords.longitude
+            });
+            props.onLocationPicked({
                 lat: location.coords.latitude,
                 lng: location.coords.longitude
             });
